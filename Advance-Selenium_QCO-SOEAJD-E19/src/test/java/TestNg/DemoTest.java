45fdcf18d46ed25caf3715739818cceec2f57db5
sanjaygit1 <rathinreddy1402@gmail.com>
2025-04-15 19:45:07 +0530

    push1

---
+++ Advance-Selenium_QCO-SOEAJD-E19/src/test/java/TestNg/DemoTest.java
@@ -0,0 +1,18 @@
+package TestNg;
+
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import Generic_Utilities.BaseClass;
+
+@Listeners(Generic_Utilities.ListenersImp.class)
+public class DemoTest extends BaseClass {
+
+	
+	@Test
+	public void m1()
+	{
+	//	Assert.fail();
+	}
+}
